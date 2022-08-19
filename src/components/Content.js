24178f27e867315551da64eb5778bc945965b608
Katie <kzumpano93@gmail.com>
2022-08-19 10:38:32 -0600

    Updated components and css, need to fix image link

---
+++ src/components/Content.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+
+
+
+export default function Content() {
+    return (
+        <>
+     
+        <div className="btns" >
+        <button className="email-button">Email</button>
+        
+        <button className="li-button">LinkedIn</button>
+        </div>
+        
+        <div className="about-int">
+       
+        <div className="about">
+         <h1>About</h1>
+        <p> I'm a Full stack Java Developer with a passion for Front End design, creative problem solving, and buckets of coffee. Resident of the PNW, mom to a small dog with a big attitude, and enthusiastic about tech!</p>
+        </div>
+        
+        <div className="interests">
+        <h1>Interests</h1>
+        <p>Some of my personal passions and priorities include advocacy, reducing environmental impact, and protecting human rights. My dream job would be for a company that values inclusivity and diversity in their hiring practices, and allows me use my development skills in a collaborative team setting that reflects those same passions and priorities. When I'm not working on projects, I love getting outdoors in the great Pacific Northwest, vegan pizza, and hanging out with my dog.</p>
+        </div>
+        </div>
+     
+        </>
+        
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+
+
+export default function Header() {
+    return (
+        <>
+        <div className="picture">
+        <img src='./assets/head-shot.jpg' alt="main"/>
+        </div>
+        <div className="caption">
+        <h1>Katie Zumpano</h1>
+        <h2>Full Stack Developer</h2>
+        <h3>kzump.com</h3>
+        </div>
+       
+        </>
+        
+    )
+}
